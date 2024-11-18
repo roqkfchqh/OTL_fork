9165bf1084fabb9a6da2801c03e9edb4b333c792
youn99 <67899848+roqkfchqh@users.noreply.github.com>
2024-11-18 19:48:55 +0900

    gradle_first_commit

---
+++ otl-server/src/test/java/com/example/otl_server/OtlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otl_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
